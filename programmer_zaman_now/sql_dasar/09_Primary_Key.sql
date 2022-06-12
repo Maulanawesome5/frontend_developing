a96e989788af0ee56e7f5c455dcf919882267b00
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-12 22:33:06 +0700

    ProgrammerZamanNow : Primary key

---
+++ programmer_zaman_now/sql_dasar/09_Primary_Key.sql
@@ -0,0 +1,19 @@
+SHOW DATABASES; -- untuk melihat daftar database yang tersedia
+CREATE DATABASE belajar_mariadb; -- Membuat database
+USE belajar_mariadb; -- untuk megakses database
+SHOW TABLES; -- untuk melihat isi tabel di dalam database
+
+SHOW ENGINES; -- Menampilkan STORAGE ENGINE
+
+
+
+-- Primary Key tabel products
+ALTER TABLE products
+    ADD PRIMARY KEY (id);
+
+DESCRIBE products;
+SELECT * FROM products;
+
+
+
+
