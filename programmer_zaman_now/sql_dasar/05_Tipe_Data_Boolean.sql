58b09849b9bd9a880faf16155fd1fd78e3cb28f0
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-12 19:17:23 +0700

    ProgrammerZamanNow : Tipe data Boolean

---
+++ programmer_zaman_now/sql_dasar/05_Tipe_Data_Boolean.sql
@@ -0,0 +1,12 @@
+SHOW DATABASES; -- untuk melihat daftar database yang tersedia
+CREATE DATABASE belajar_mariadb; -- Membuat database
+USE belajar_mariadb; -- untuk melihat isi di dalam database
+SHOW TABLES; -- untuk melihat isi tabel di dalam database
+
+
+-- Tipe data Boolean
+-- True dan False
+
+
+
+
