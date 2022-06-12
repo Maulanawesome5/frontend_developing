2d3166b36cc859b9cf547e4107ec372f9d7df6a0
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-12 19:14:27 +0700

    ProgrammerZamanNow : Tipe data Date and Time

---
+++ programmer_zaman_now/sql_dasar/04_Tipe_Data_DateTime.sql
@@ -0,0 +1,17 @@
+SHOW DATABASES; -- untuk melihat daftar database yang tersedia
+CREATE DATABASE belajar_mariadb; -- Membuat database
+USE belajar_mariadb; -- untuk melihat isi di dalam database
+SHOW TABLES; -- untuk melihat isi tabel di dalam database
+
+
+-- Tipe data Date and Time
+-- Jenis        Format                  Contoh
+-- DATE         YYYY-MM-DD              2020-10-10
+-- DATETIME     YYYY-MM-DD HH:MM:SS     2020-10-10 10:10:10
+-- TIMESTAMP    YYYY-MM-DD HH:MM:SS     2020-10-10 10:10:10
+-- TIME         HH:MM:SS                10:10:10
+-- YEAR         YYYY                    2020
+
+
+
+
