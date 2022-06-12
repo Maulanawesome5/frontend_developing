c1384fa47e0e10393c9808f6bde52cda8cc66acd
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-12 17:22:39 +0700

    ProgrammerZamanNow : Membuat database

---
+++ programmer_zaman_now/sql_dasar/01_Membuat_Database.sql
@@ -0,0 +1,10 @@
+SHOW DATABASES; -- untuk melihat daftar database yang tersedia
+
+-- Membuat database
+CREATE DATABASE belajar_mariadb;
+
+-- untuk melihat isi di dalam database
+USE belajar_mariadb;
+
+-- untuk melihat isi tabel di dalam database
+SHOW TABLES;
