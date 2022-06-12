c1ddbc97963eb231e99d4c6d7a5ac570df78d3b5
Maulanawesome5 <maulanawesome5@gmail.com>
2022-06-12 21:13:21 +0700

    ProgrammerZamanNow : Operator advance di JS

---
+++ programmer_zaman_now/JavaScript_Dasar/js/24_Operator_Logika.js
@@ -0,0 +1,7 @@
+// Operator OR
+console.info("hello" || '');
+console.info("" || []);
+console.info("0" || "NOL");
+console.info(0 || "NOL");
+console.info(null || "null");
+console.info(undefined || "undefined")
